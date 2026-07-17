1e7cbc12816c30f4882d601f404448f112305d79
GideonJulian <gideonchinonso77@gmail.com>
2026-07-17 12:30:18 +0100

    update: Currency selector added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,7 +64,7 @@ export default function Navbar() {
             ))}
           </div>
 
-          {/* <CurrencySelector /> */}
+          <CurrencySelector />
 
           <motion.div
             whileTap={{
